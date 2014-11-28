785074b7607b18465010385717d949b07a05b97e
PFei_He <498130877@qq.com>
2014-11-28 17:26:19 +0800

    修改了名字过长的对象和方法，删减了不必要的变量和对象

--- PFCarouselView/PFCarouselView.h
+++ PFCarouselView/PFCarouselView.h
@@ -7,7 +7,7 @@
 //
 //  https://github.com/PFei-He/PFCarouselView
 //
-//  vesion: v0.3.0
+//  vesion: v0.4.0
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
