e599be17a27ec6ba91deb34df42cc850f8e62204
PFei_He <498130877@qq.com>
2014-11-28 15:51:07 +0800

    添加了版本号

--- PFCarouselView/PFCarouselView.h
+++ PFCarouselView/PFCarouselView.h
@@ -7,6 +7,8 @@
 //
 //  https://github.com/PFei-He/PFCarouselView
 //
+//  vesion: v0.3.0
+//
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
 //  in the Software without restriction, including without limitation the rights
